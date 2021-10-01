058b086c54798fe11f5811944b7838a633b90b47
Mohit Varikuti <65371337+GitHubEmploy@users.noreply.github.com>
2021-09-30 23:18:09 -0700

    Update deep_daze.py

--- deep_daze.py
+++ deep_daze.py
@@ -22,6 +22,13 @@ from tqdm import trange, tqdm
 
 from clip import load, tokenize
 
+import torch_xla
+import torch_xla.core.xla_model as xm
+
+second_dev = xm.xla_device(n=2, devkind='TPU')
+t2 = torch.zeros(3, 3, device = second_dev)
+print(t2)
+
 
 # Helpers
 
